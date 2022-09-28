a953f131c653a65853c730be8a4ca6ae4c85d7e7
aashutosh <aashutosh.poudel8@gmail.com>
2022-09-28 09:43:53 +0545

    Implemented coroutines in viewmodel scope

--- Mealz App/app/src/main/java/com/infoshapers/mealzapp/ui/meals/MealsCategoriesViewModel.kt
+++ Mealz App/app/src/main/java/com/infoshapers/mealzapp/ui/meals/MealsCategoriesViewModel.kt
@@ -3,6 +3,7 @@ package com.infoshapers.mealzapp.ui.meals
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.infoshapers.model.MealsRepository
 import com.infoshapers.model.response.MealResponse
 import com.infoshapers.model.response.MealsCategoriesResponse
@@ -13,11 +14,9 @@ import kotlinx.coroutines.launch
 
 class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRepository()) :
     ViewModel() {
-    private val mealsJob = Job()
 
     init {
-        val scope = CoroutineScope(mealsJob + Dispatchers.IO)
-        scope.launch() {
+        viewModelScope.launch() {
             val meals = getMeals()
             mealsState.value = meals
         }
@@ -26,12 +25,7 @@ class MealsCategoriesViewModel(private val repository: MealsRepository = MealsRe
     val mealsState: MutableState<List<MealResponse>> = mutableStateOf(emptyList<MealResponse>())
 
 
-    override fun onCleared() {
-        super.onCleared()
-        mealsJob.cancel()
-    }
-
-    suspend fun getMeals(): List<MealResponse> {
+    private suspend fun getMeals(): List<MealResponse> {
         return repository.getMeals().categories
     }
 }
